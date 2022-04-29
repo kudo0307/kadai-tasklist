7b4abec30abe0aebc566719da70831809088d31e
seed1371 <bakatesu0307@gmail.com>
2022-04-30 00:09:09 +0900

    EditServlet修正

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -13,24 +13,14 @@ import javax.servlet.http.HttpServletResponse;
 import models.Task;
 import utils.DBUtil;
 
-/**
- * Servlet implementation class EditServlet
- */
 @WebServlet("/edit")
 public class EditServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public EditServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
@@ -43,7 +33,9 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("_token", request.getSession().getId());
 
         // タスクIDをセッションスコープに登録
-        request.getSession().setAttribute("task_id", t.getId());
+        if(t != null) {
+            request.getSession().setAttribute("task_id", t.getId());
+        }
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
